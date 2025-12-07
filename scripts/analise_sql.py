15a6e5394527bbefb9370d9e74ca855ac2e81d28
Thiago Prado <thiagoprado1499@gmail.com>
2025-12-07 02:05:49 -0300

    Ajuste como api

--- scripts/analise_sql.py
+++ scripts/analise_sql.py
@@ -1,35 +1,47 @@
 import pandas as pd
 from sqlalchemy import create_engine
+import os
+from dotenv import load_dotenv
+import urllib.parse
 
-engine = create_engine('sqlite:///doare.db')
-
-query_sql = """
-SELECT 
-    b.email,
-    b.primeiro_nome_tratado AS primeiro_nome,
-    b.data_de_registro,
-    b.data_de_pagamento,
-    b.status,
-    t.Descrição AS tipo_doacao_descricao,
-    b.forma_de_pagamento,
-    b.canal_tratado AS canal_origem,
-    d.valor_bruto AS valor
-FROM tabela_base b
-LEFT JOIN tabela_dados_doacao d 
-    ON b.email = d.email
-LEFT JOIN tabela_tipo t 
-    ON d.CD_Tipo = t.CD_Tipo
-LEFT JOIN tabela_canal c 
-    ON b.canal_tratado = c.Canal
-"""
+load_dotenv()
 
 try:
+    db_password = os.getenv("DATABASE_PASSWORD")
+    if not db_password:
+        raise ValueError("Senha não encontrada no .env")
+
+    p_password = urllib.parse.quote_plus(db_password)
+    host = os.getenv("HOST")
+    port = os.getenv("PORT")
+    db_name = os.getenv("DB_NAME")
+    user = os.getenv("USER")
+
+    db_string = f"postgresql://{user}:{p_password}@{host}:{port}/{db_name}"
+    engine = create_engine(db_string)
+
+    query_sql = """
+    SELECT 
+        b.email,
+        b.primeiro_nome_tratado AS primeiro_nome,
+        b.data_de_registro,
+        b.status,
+        t."Descrição" AS tipo_doacao,
+        b.canal_tratado AS canal,
+        d.valor_bruto AS valor
+    FROM tabela_base b
+    LEFT JOIN tabela_dados_doacao d ON b.email = d.email
+    LEFT JOIN tabela_tipo t ON d."CD_Tipo" = t."CD_Tipo"
+    """
+
     df_final = pd.read_sql(query_sql, engine)
     
-    print("Cruzamento de dados realizado com sucesso!")
+    print("Query executada com sucesso!")
+    print(df_final.head())
 
-    total_valor = df_final['valor'].sum()
-    print(f"\nValor Total Soma: R$ {total_valor:,.2f}")
+    print("\n--- Totais para Conferência ---")
+    print(f"Valor Total: R$ {df_final['valor'].sum():,.2f}")
+    print(f"Total de Doadores Únicos: {df_final['email'].nunique()}")
 
 except Exception as e:
-    print(f"Erro ao executar a consulta SQL: {e}")
+    print(f"Erro na execução da query: {e}")
